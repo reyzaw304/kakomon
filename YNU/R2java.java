0f40390524067e27feba2aec00e0ba88345e1b31
maijajaja <reyzaw304@gmail.com>
2023-05-07 19:11:25 +0900

    soal java

--- YNU/R2java.java
+++ YNU/R2java.java
@@ -1,3 +1,43 @@
-public class R2java {
+public class ListMain {
+	private static final int EOD = -1;
+	private static int i;
+	private static void resetData() {1=0};
+	private static int getData() { return A[i++];}
 
+	public static void main(String[] args) {
+		Node startNode, newNode, hereNode;
+		NodeCount startNodeCount, newNodeCount, hereNodeCount;
+		int key;
+
+		resetData();
+		startNode= new Node(getData());
+		while((key=getData())!=EOD) {
+			newNode=new Node(key);
+			startNode.searchinsert(newNode);
+		}
+		startNode.print();
+
+		resetData();
+		startNodeCount= new NodeCount(getData());
+		while((key=getData()) != EOD) {
+			newNodeCount = new NodeCount(key);
+			startNodeCount.searchinsert(newNodeCount);
+		}
+		startNodeCount.print();
+
+		if((hereNodeCount=(NodeCount)Node.search(startNodeCount, 5))!=null) hereNodeCount.deletenext();
+		startNodeCount.print();
+	}
 }
+
+public class Node(
+	private int key;
+	private Node next;
+
+	public Node(int key) {
+		this.key=key;
+		this.next=null;
+	}
+
+	public int key() {return this.key;}
+)
